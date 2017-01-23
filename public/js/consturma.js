62095eb4648c9488721382812cb7e6e3f1dea170
Yurr Soriedem <yurimedeiros33@gmail.com>
2017-01-23 20:29:20 -0200

    ajustes ConsultaTUrma

--- public/js/consturma.js
+++ public/js/consturma.js
@@ -1,5 +1,6 @@
 
     $(function(){
+      $('#textCont').hide();
       $('#textAreas').hide();
       $('#btAreas').hide();
       $.ajax({
@@ -43,25 +44,31 @@
                 .append($("<td />").val(data[i][1]).text(data[i][1]))
                 .append($("<td />").val(data[i][0]).text(data[i][0]))
                 .append($("<td />").val(data[i][2]).text(data[i][2]))
-                .append("<input type='checkbox' id='chk' name='chk'  /> ")
-                .append($("<td  />")
-                  ));
+                // // .append($("<td  />")
+                // // .append("<input type='checkbox' id='chk' name='chk'  /> ")
+                //
+                //   )
+              );
+              // $( "<b>Deseja contatar essa turma?</b>" ).appendTo( "body" );
               console.log(data[i]);
             }
+            $("tbody").append("<br>");
+            $("tbody").append("Deseja contatar essa turma? <input type='checkbox' id='chk' name='chk'  /> ");
             console.log('process sucess');
           },
           error: function() { console.log('process error');},
         });
       });
     });
-
     $(document).on('change', '[type=checkbox]',function(e){
       var umaChecked = $('input[name="chk"]:checked').length > 0;
       if (umaChecked){ //NAO TA CERTO MASSSSSSSSSS FUNCIONA
+        $('#textCont').show();
         $('#textAreas').show();
         $('#btAreas').show();
       }
       else{
+        $('#textCont').hide();
         $('#textAreas').hide();
         $('#btAreas').hide();
       }
