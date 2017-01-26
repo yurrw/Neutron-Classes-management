69931bed4ef9427f1687c5b362ab5ad9062c0c8b
Yurr <yurimedeiros33@gmail.com>
2017-01-26 11:57:42 -0200

    ajustes

--- controllers/CadController.js
+++ controllers/CadController.js
@@ -241,34 +241,57 @@ exports.pesquisaQuest = function(request, response){
 };
 
 exports.cadastroDiario  = function(request,response, next){
-  var data          = request.body.txtSelectedDate;
-  var datasplit     = data.split("/");
-  var dataformatada = datasplit[2] +'-'+ datasplit[1] +'-'+ datasplit[0];
-  var confirm       = 0;
+   var datasplit     = data.split("/");
+   var dataformatada = datasplit[2] +'-'+ datasplit[1] +'-'+ datasplit[0];
+   var confirm       = 0;
   var qry           = "INSERT INTO prof_diario(matricula,turma,data,disciplina_id,comentario) SELECT '"+request.body.matriculaProf+"','"+request.body.turma+"','"+dataformatada+"',disciplina_id , '"+request.body.comentario +"' from disciplinas where disciplina_nome= '"+request.body.disciplina+"' ";
+ var qry           = "INSERT INTO prof_diario(matricula,turma,data,disciplina_id,comentario) SELECT '"+request.user.matricula+"','"+request.body.turma+"','"+dataformatada+"',disciplina_id , '"+request.body.comentario +"' from disciplinas where disciplina_nome= '"+request.body.disciplina+"' ";
+  var qryDEL        = "DELETE FROM prof_diario WHERE turma = '"+request.body.turma+"' AND matricula = '"+request.user.matricula+"' AND disciplina_id = '"+request.body.nomedisciplina+"' AND data = '"+dataformatada+"' AND comentario = '"+request.body.textAreas+"'";
+  var qry2DEL       = "DELETE FROM prof_diario_aluno WHERE cod_aula IN (SELECT cod_aula FROM prof_diario WHERE turma = '"+request.body.turma+"' AND matricula = '"+request.user.matricula+"' AND disciplina_id = '"+request.body.nomedisciplina+"' AND data = '"+dataformatada+"' AND comentario = '"+request.body.textAreas+"'";
+  console.log(qryDEL);
+  console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+  console.log(qry2DEL);
+  console.log("------------------------------------");
+
+  connDB.query(qryDEL,function(err,rows){
+    if (err)
+        console.log(err);
 
 
-    connDB.query(qry,function(err,rows){
-      if (err)
-          console.log(err);
 
-        confirm=1;
-        if(confirm==1){
-          for(var i=0; i<request.body.matriculas.length; i++){
+});
+ 
+connDB.query(qry2DEL,function(err,rows){
+  if (err)
+      console.log(err);
+
+});
+ 
+     connDB.query(qry,function(err,rows){
+       if (err)
+           console.log(err);
+ 
+          console.log("chegou aqui");
+
+         confirm=1;
+
+
+         if(confirm==1){
+           for(var i=0; i<request.body.matriculas.length; i++){
                var qry2=" INSERT INTO prof_diario_aluno (cod_aula,matricula,presente) SELECT prof_diario.cod_aula,'"+request.body.matriculas[i]+"', '"+request.body.chks[i]+"'  FROM   prof_diario   WHERE   prof_diario.matricula ='"+request.body.matriculaProf+"'                            AND     prof_diario.data='"+dataformatada+"'  AND     prof_diario.turma='"+request.body.turma+"'"
+ 
+               var qry2=" INSERT INTO prof_diario_aluno (cod_aula,matricula,presente) SELECT prof_diario.cod_aula,'"+request.body.matriculas[i]+"', '"+request.body.chks[i]+"'  FROM   prof_diario   WHERE   prof_diario.matricula ='"+request.user.matricula+"'                            AND     prof_diario.data='"+dataformatada+"'  AND     prof_diario.turma='"+request.body.turma+"'"
+               console.log(qry2);
+                   connDB.query(qry2,function(err,rows){
+                     if(err){
+                   console.log('Error connecting to Db');
+                   return;
+                   }
+                   console.log('Connection established');
 
-                  connDB.query(qry2,function(err,rows){
-                    if(err){
-                  console.log('Error connecting to Db');
-                  return;
-                  }
-                  console.log('Connection established');
-                      });
-                  }
-        }
-  });
-
-};
+                       });
+                   }
+         }
 
 
 
